ff653cc4eafe5090f0db198cf242d86b9b0a5d77
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-06-29 21:37:18 +0300

    Add project files.

---
+++ FinancialApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinancialApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    }
+}
---
+++ FinancialApp/Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialApp.Models
+{
+    public class Comment
+    {
+        public Guid? StockId { get; set; }
+        //Navigation property
+        public Stock? Stock { get; set; }
+    }
+}
