cc79a1aaca52b342cc4e64ffa611a4b65f1285cd
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-07-03 20:12:13 +0300

    Pagination

--- FinancialApp/Repository/StockRepository.cs
+++ FinancialApp/Repository/StockRepository.cs
@@ -59,7 +59,10 @@ namespace FinancialApp.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
 
