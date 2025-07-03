6cb0e11ed4c0b1a4b3e9af348b3782c87a39624b
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-07-03 20:09:24 +0300

    Sorting

--- FinancialApp/Helpers/QueryObject.cs
+++ FinancialApp/Helpers/QueryObject.cs
@@ -4,9 +4,9 @@
     {
         public string? Symbol { get; set; } = null;
         public string? CompanyName { get; set; } = null;
-        /*public string? SortBy { get; set; } = null;
+        public string? SortBy { get; set; } = null;
         public bool IsDecsending { get; set; } = false;
-        public int PageNumber { get; set; } = 1;
+        /*public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 20;*/
     }
 }
