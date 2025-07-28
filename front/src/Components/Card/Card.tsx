1e7baaebbdec910e7c77031e3e1ba48ae14b1474
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-07-28 23:02:37 +0300

    JSX

---
+++ front/src/Components/Card/Card.tsx
@@ -0,0 +1,18 @@
+import './Card.css'
+
+const Card = () => {
+  return (
+    <div className="card">
+        <img src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg" alt="Image" />
+
+        <div className="details">
+            <h2>APl`</h2>
+            <p>100&</p>
+
+        </div>
+    </div>
+    
+  )
+}
+
+export default Card;
