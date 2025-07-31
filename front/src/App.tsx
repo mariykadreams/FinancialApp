07e443cf08cb359a6c5e9bcfea128af6713f4ca8
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-07-31 14:40:58 +0300

    key chnage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,17 +2,28 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css'
 import CardList from './Components/CardList/CardList'
 import Search from './Components/Search/Search'
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
-const [search, setSearch] = useState<string>("");
+  const [search, setSearch] = useState<string>("");
+  const[searchResult, setSearchResult] = useState<CompanySearch[]>([])
+  const[serverError, setServevrError] = useState<string>("");
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> ) =>{
         setSearch(e.target.value);
         console.log(e);
     };
 
-    const onClick = (e: SyntheticEvent) =>{
-        console.log(e);
+    const onClick = async (e: SyntheticEvent) =>{
+        const result = await searchCompanies(search);
+        if(typeof result === "string"){
+          setServevrError(result);
+        }else {
+
+          setSearchResult(result);
+        }
+        console.log("Search Result:",searchResult);
     };
 
   return (
