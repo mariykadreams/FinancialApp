ca14a5b26b15571ff885c6c9da314f3885c4a1ba
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-08-03 17:10:43 +0300

    React Router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,61 +1,15 @@
 import { ChangeEvent, SyntheticEvent, useState } from 'react';
-import CardList from './Components/CardList/CardList'
-import Search from './Components/Search/Search'
-import { CompanySearch } from './company';
-import { searchCompanies } from './api';
-import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/hero';
+import { Outlet } from 'react-router';
+
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const[searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const[serverError, setServevrError] = useState<string>("");
-
-    const handleSearchChange = (e: ChangeEvent<HTMLInputElement> ) =>{
-        setSearch(e.target.value);
-    };
-
-    const onPortfolioCreate = (e: any) =>{
-      e.preventDefault();
-      const exists = portfolioValues.find((value) => value === e.target[0].value);
-      if(exists) return;
-      const updatedPortfolio = [...portfolioValues, e.target[0].value];
-      setPortfolioValues(updatedPortfolio);
-    }
-
-    const onPortfolioDelete = (e: any) => {
-      e.preventDefault();
-      const removed = portfolioValues.filter((value) =>{
-        return value !== e.target[0].value;
-      });
-      setPortfolioValues(removed);
-    }
-
-
-    const onSearchSubmit = async (e: SyntheticEvent) =>{
-      e.preventDefault();
-        const result = await searchCompanies(search);
-        if (typeof result === "string") {
-          setServevrError(result);
-        } else if (Array.isArray(result)) {
-          setSearchResult(result);
-        }
-
-        console.log("Search Result:",searchResult);
-    };
-
+  
   return (
-    <div className='App'>
-      <Navbar />
-      <Hero />
-      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
-      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
-
-      {serverError && <h1>Unable to connect to API{serverError}</h1>}
-    </div>
+   <>
+   <Navbar />
+   <Outlet />
+   </>
   )
 }
 
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { searchCompanies } from './api'
+import { RouterProvider } from 'react-router-dom'
+import React from 'react'
+import { router } from './Routers/Routes'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
 )
 
 console.log(searchCompanies("tsla"));
