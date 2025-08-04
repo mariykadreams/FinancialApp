c5d2e470a18168ab8e6d97752782636755a943a0
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-08-04 21:54:45 +0300

    Services

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,8 @@
 import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
    <>
    <Navbar />
    <Outlet />
+   <ToastContainer />
    </>
   )
 }
