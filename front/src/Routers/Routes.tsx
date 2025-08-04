a75fdafe98b75caaf7b168dc17ce88fccfd76f8d
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-08-04 13:07:11 +0300

    Cash Flow Statement

--- front/src/Routers/Routes.tsx
+++ front/src/Routers/Routes.tsx
@@ -7,6 +7,7 @@ import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
+import CashflowStatement from "../Components/CashflowStatement/CashflowStatement";
 
 export const router = createBrowserRouter([
   {
@@ -15,14 +16,15 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
-      {path:"design-guide", element: <DesignGuide />},
+      { path: "design-guide", element: <DesignGuide /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
         children: [
           { path: "company-profile", element: <CompanyProfile /> },
           { path: "income-statement", element: <IncomeStatement /> },
-           { path: "balance-sheet", element: <BalanceSheet /> }
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashflowStatement /> },
         ],
       },
     ],
