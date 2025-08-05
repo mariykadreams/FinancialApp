b4e1432905dc2c836376e81b2e47eb0c7ae8feb9
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-08-05 13:41:59 +0300

    comments

---
+++ front/src/Services/CommentService.tsx
@@ -0,0 +1,22 @@
+
+import axios from "axios";
+import { CommentPost } from "../Models/Comment";
+import { handleError } from "../Helpers/ErrorHandler";
+
+const api = "https://localhost:7104/api/comment/";
+
+export const commentPostAPI = async (
+  title: string,
+  content: string,
+  symbol: string
+) => {
+  try {
+    const data = await axios.post<CommentPost>(api + `${symbol}`, {
+      Title: title,
+      Content: content,
+    });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
